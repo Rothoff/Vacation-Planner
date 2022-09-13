32bba3931a70bf7fc58e653023e98e1a3da0f45a
Erik Hollander <erik.hollander@kambi.com>
2022-09-13 12:15:22 +0200

    initial

---
+++ src/test/java/com/example/vacationplanner/VacationPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacationplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
