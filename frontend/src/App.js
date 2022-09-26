9591406cd41a914110a3949e9427901f9aa0811b
Fredrik Rothoff <fredrik.rothoff@kambi.com>
2022-09-26 19:09:31 +0200

    Added some frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
-import Employee from './components/Employee'
-import MyAppBar from './components/MyAppbar'
-import StapleChart from './components/Chart.jsx'
-import React from 'react'
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Sidebar from './components/Sidebarcomponents/Sidebar';
+import { BrowserRouter, Routes} from "react-router-dom";
+import Dashboard from './pages/Dashboard';
+import { Employees, AddEmployee } from './pages/Employees';
+import {Team, AddTeam } from './pages/Team';
 
 function App() {
   return (
-    <div className="App">
-
-      <MyAppBar />
-      <StapleChart/>
-
-    </div>
+   
+    <BrowserRouter>
+      <Sidebar/>
+        <Routes>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/employees' element={<Employees/>}/>
+        <Route path='/employees/addEmployee' element={<AddEmployee/>}/>
+        <Route path='/team' element={<Team/>}/>
+        <Route path='/team/addTeam' element={<AddTeam/>}/>
+        </Routes>
+    </BrowserRouter>
+    
+    
   );
 }
 
